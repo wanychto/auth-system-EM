58e7354c89884fcaec1c95476487279c1ed49986
wanychto <smirnovaa2500@gmail.com>
2026-02-04 00:24:56 +0300

    Изменены модели в приложении access
    Внесены некоторые изменения в access/models.py и применены миграции

--- access/models.py
+++ access/models.py
@@ -3,26 +3,28 @@ from users.models import User
 
 class Roles(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 class User_role(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Roles, on_delete=models.PROTECT)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together =("user", "role")
 
 class Business_elements(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 class Access_rules(models.Model):
-    id = models.CharField(max_length=128, unique=True)
-    role = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
     element_id = models.ForeignKey(Business_elements, on_delete=models.CASCADE)
-    read_permission = models.BooleanField()
-    read_all_permission = models.BooleanField()
-    create_permission = models.BooleanField()
-    update_permission = models.BooleanField()
-    update_all_permission = models.BooleanField()
-    delete_permission = models.BooleanField()
-    delete_all_permission = models.BooleanField()
+    read_permission = models.BooleanField(default=False)
+    read_all_permission = models.BooleanField(default=False)
+    create_permission = models.BooleanField(default=False)
+    update_permission = models.BooleanField(default=False)
+    update_all_permission = models.BooleanField(default=False)
+    delete_permission = models.BooleanField(default=False)
+    delete_all_permission = models.BooleanField(default=False)
 
